b248cc9c034cd0caf2b75990d805c26a751d5e5a
abcde <276556803@qq.com>
2017-11-15 09:14:06 +0800

    im 客服

--- lib/Pub/ChuanHaiIm.php
+++ lib/Pub/ChuanHaiIm.php
@@ -18,7 +18,62 @@ class ChuanHaiIm
     {
         $UserData=array('userid'=>"{$UserId}",'domainid'=>self::DomainID().'','name'=>"{$UserName}",'pic'=>"{$UserPic}");
         $UserData['key']=self::ApiSign($UserData,self::Key());
-        self::SetCookies('chuanhaisoft_im_user',base64_encode(json_encode($UserData)),$_Expire==null?365*24*3600:$_Expire);
+        self::SetCookies('chuanhaisoft_im_user',base64_encode(json_encode($UserData)),$_Expire==null?5*24*3600:$_Expire);
+    }
+    
+    public static function SetKeFu($UserName,$UserPic,$WelcomeMess=null)
+    {
+        $Len=15;
+        $UserId=self::GetIp();
+        if(strlen($UserId)>8)
+            $UserId=substr($UserId, 0,8);
+
+        if(strlen($UserId)<$Len)
+            $UserId="{$UserId}".self::RandStr($Len-strlen($UserId));
+        
+        if(!isset($_COOKIE['chuanhaisoft_im_user']) || !$_COOKIE['chuanhaisoft_im_user'] || strlen($_COOKIE['chuanhaisoft_im_user'])<=5 )
+        {
+            $UserData=array('userid'=>"{$UserId}",'domainid'=>self::DomainID().'','name'=>"{$UserName}",'pic'=>"{$UserPic}");
+            if($WelcomeMess && $WelcomeMess!='')
+                $UserData['welcome_mess']=$WelcomeMess;
+            
+            $UserData['key']=self::ApiSign($UserData,self::Key());
+            self::SetCookies('chuanhaisoft_im_user',base64_encode(json_encode($UserData)),10*365*24*3600);
+        }
+    }
+    
+    private static function RandStr($len=6,$format='NUMBER')
+    {
+        $chars='0123456789';
+        $password="";
+        while(strlen($password)<$len)
+            $password.=substr($chars,(mt_rand()%strlen($chars)),1);
+        return $password;
+    }
+    
+    private static function GetIp()
+    {
+        $ip=empty($_SERVER['HTTP_X_FORWARDED_FOR'])?$_SERVER['REMOTE_ADDR']:$_SERVER['HTTP_X_FORWARDED_FOR'];
+        if(is_numeric(strpos($ip, ',')))
+        {
+            $ip=explode(',', $ip);
+            $ip=trim($ip[0]);
+        }
+        return self::findNum($ip);
+    }
+    
+    private static function findNum($str='')
+    {
+        $str=trim($str);
+        if(empty($str)){return '';}
+        $temp=array('1','2','3','4','5','6','7','8','9','0');
+        $result='';
+        for($i=0;$i<strlen($str);$i++){
+            if(in_array($str[$i],$temp)){
+                $result.=$str[$i];
+            }
+        }
+        return $result;
     }
     
     private static function SetCookies($_CookieName,$_Value,$_Expire=0)

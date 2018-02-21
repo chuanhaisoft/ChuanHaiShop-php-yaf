61b9287d60a8582328dfd8f5e49db3340fa13089
abcde <276556803@qq.com>
2018-02-21 19:26:48 +0800

    set catchException  false

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 define('APP_PATH', __DIR__.'/protected');
 $app = new \Yaf\Application(APP_PATH . "/config/application.ini");
-$app->getDispatcher()->catchException(true);
+$app->getDispatcher()->catchException(false);
 $app->bootstrap()->run();

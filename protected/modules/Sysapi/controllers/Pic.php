566463c089bbf241492624ffd9e0f53f44146e03
abcde <276556803@qq.com>
2018-04-27 11:53:26 +0800

    fix where

--- protected/modules/Sysapi/controllers/Pic.php
+++ protected/modules/Sysapi/controllers/Pic.php
@@ -7,7 +7,7 @@ class PicController extends \Base\Api
 	public function Lun_boAction()
 	{
 	    $Total=0;
-        $rows=\Bll\News::GetList(1, 10,['SORT_ID=10 and STATE=1'],$Total,'ID,TITLE,PIC1,ADD_TIME');
+        $rows=\Bll\News::GetList(1, 10,'SORT_ID=10 and STATE=1',$Total,'ID,TITLE,PIC1,ADD_TIME');
         echo parent::ToJson($rows);
 	}
 	
